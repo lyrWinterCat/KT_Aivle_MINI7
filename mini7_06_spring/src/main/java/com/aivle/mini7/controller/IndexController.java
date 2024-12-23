1db7cb8d7407e16ba0d3eee36e5a0b2f94a6483b
lyrWinterCat <103623292+lyrWinterCat@users.noreply.github.com>
2024-12-23 14:56:26 +0900

    commit : mustache 성공~~
    예에

--- mini7_06_spring/src/main/java/com/aivle/mini7/controller/IndexController.java
+++ mini7_06_spring/src/main/java/com/aivle/mini7/controller/IndexController.java
@@ -6,6 +6,7 @@ import com.aivle.mini7.client.dto.HospitalResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,9 +21,16 @@ public class IndexController {
 
     private final FastApiClient fastApiClient;
 
+//    @GetMapping("/")
+//    public String index() {
+//        return "index";
+//    }
+
     @GetMapping("/")
-    public String index() {
-        return "emergency";
+    public String home(Model model) {
+        model.addAttribute("title", "Bootstrap Test Page");
+        model.addAttribute("username", "LYR");
+        return "index";
     }
 
     @PostMapping("/recommend_hospital")
