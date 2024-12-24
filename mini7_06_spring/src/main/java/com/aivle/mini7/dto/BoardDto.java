b43beb27a005633755b397363f5c8f2e0e8fc212
DH <ahnct007@gmail.com>
2024-12-24 15:46:41 +0900

    게시판 update

--- mini7_06_spring/src/main/java/com/aivle/mini7/dto/BoardDto.java
+++ mini7_06_spring/src/main/java/com/aivle/mini7/dto/BoardDto.java
@@ -14,31 +14,40 @@ public class BoardDto {
 	private String updateTime;
 	private String password;
 	private String username;
-	
-    @Getter
-    @Setter
-    @ToString
-    @NoArgsConstructor
-    public static class Post {
-        private String title;
-        private String content;
+
+	@Getter
+	@Setter
+	@ToString
+	@NoArgsConstructor
+	public static class Post {
+		private String title;
+		private String content;
 		private String password;
 		private String username;
 
-        // 명시적으로 전체 필드 생성자 정의
-        public Post(String title,
+		// 명시적으로 전체 필드 생성자 정의
+		public Post(String title,
 					String content,
 					String password,
 					String username) {
 
-            this.title = title;
-            this.content = content;
+			this.title = title;
+			this.content = content;
 			this.password = password;
 			this.username = username;
-        }
-    }
-}
+		}
+	}
 
+	@Getter
+	@Setter
+	@ToString
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class Update {
+		private String title;
+		private String content;
+	}
+}
 //public class BoardDto{
 //	@Getter
 //	@Setter
